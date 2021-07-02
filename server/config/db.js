22336707defe542825112eb2105f4b5bec8ee5e7
Carlos Ivan Mercado Marin <karlos123@junhotmail.com>
2021-07-02 09:48:11 -0500

    Servidor local comentado

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 if (process.env.NODE_ENV === "dev") {
   process.env.URLDB =
     "mongodb+srv://admin:admin1234@arkusnexus.5ihjp.mongodb.net/ArkusNexus?retryWrites=true&w=majority";
+  // "mongodb://localhost:27017/arkusnexus"
 } else {
   process.env.URLDB =
     "mongodb+srv://admin:admin1234@arkusnexus.5ihjp.mongodb.net/ArkusNexus?retryWrites=true&w=majority";
+  // "mongodb://localhost:27017/arkusnexus"
 }
 
 process.middlewares = [];
