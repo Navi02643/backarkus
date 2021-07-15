bd05673828cf795e786f6a6c7abec9cf7c7f80c7
Carlos Ivan Mercado Marin <karlos123@junhotmail.com>
2021-07-15 12:32:45 -0500

    cambios

--- server/routes/cards.routes.js
+++ server/routes/cards.routes.js
@@ -16,71 +16,7 @@ var options = {
 
 
 app.post('/create-pdf', async (req, res) => {
-    try {
-        username = req.query.username;
-        lastname = req.query.lastname;
-        const equipment = await equipmentmodel.aggregate([
-         
-          {
-            $lookup: {
-              from: "typeequipments",
-              localField: "IDtypeequipment",
-              foreignField: "_id",
-              as: "typeequipments",
-            },
-          },
-          {
-            $lookup: {
-              from: "users",
-              localField: "IDuser",
-              foreignField: "_id",
-              as: "user",
-            },
-          },
-          {
-            $replaceRoot: {
-              newRoot: {
-                $mergeObjects: [{ $arrayElemAt: ["$typeequipments", 0] }, "$$ROOT"],
-              },
-            },
-          },
-          {
-            $replaceRoot: {
-              newRoot: {
-                $mergeObjects: [{ $arrayElemAt: ["$user", 0] }, "$$ROOT"],
-              },
-            },
-          },
-          {
-            $match: {
-              $and: [{ username: username }, { lastname: lastname }],
-            },
-          },
-          {
-              $project: {
-                  _id: 0,
-                  tename: 1,
-                  model: 1,
-                  mark: 1,
-                  serialnumber: 1,
-                  username: 1,
-                  lastname: 1,
-                  account: 1
-              }
-          }
-        ]);
-        console.log(equipment);
-      } catch (err) {
-        res.status(500).send({
-          estatus: "500",
-          err: true,
-          msg: "Error getting equipments.",
-          cont: {
-            err: Object.keys(err).length === 0 ? err.message : err,
-          },
-        });
-      }
-    
+
     pdf.create(pdfTemplate(req.body), options).toFile(`Carta-compromiso${username+' '+lastname}.pdf`, (err) => {
         if(err) {
             res.send(Promise.reject());
