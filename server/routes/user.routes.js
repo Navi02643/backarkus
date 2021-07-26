011a7aec89c5404e0b67fd2d63a5d7c052e9b382
Miguel Salas <miguelangel.sf@outlook.com>
2021-07-25 20:05:03 -0500

    Ultimos cambios dev

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const keys = require("../middlewares/keys");
 const bcrypt = require("bcryptjs");
+const _ = require('underscore');
 const validateRegisterInput = require("../validation/register");
 const validateLoginInput = require("../validation/login");
 const app = express();
@@ -271,6 +272,73 @@ app.put("/", async (req, res) => {
   }
 });
 
+app.put("/role", async (req, res) => {
+  try {
+    const idUser = req.query.idUser;
+    if (req.query.idUser == "") {
+      return res.status(400).send({
+        estatus: "400",
+        err: true,
+        msg: "Error: A valid id was not sent.",
+        cont: 0,
+      });
+    }
+    req.body._id = idUser;
+    const Userfind = await usermodelrole.findById(idUser);
+    if (!Userfind) {
+      return res.status(404).send({
+        estatus: "404",
+        err: true,
+        msg: "Error: The user was not found in the database.",
+        cont: Userfind,
+      });
+    }
+    const newuser = new usermodelrole(req.body);
+    let err = newuser.validateSync();
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error: Error inserting user.",
+        cont: {
+          err,
+        },
+      });
+    }
+    const userupdate = await usermodelrole.findByIdAndUpdate(
+      idUser,
+      { $set: newuser },
+      { new: true }
+    );
+    if (!userupdate) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error: Trying to update the user.",
+        cont: 0,
+      });
+    } else {
+      return res.status(200).json({
+        ok: true,
+        resp: 200,
+        msg: "Success: The user was updated successfully.",
+        cont: {
+          Userfind,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      estatus: "500",
+      err: true,
+      msg: "Error: Error updating user.",
+      cont: {
+        err: Object.keys(err).length === 0 ? err.message : err,
+      },
+    });
+  }
+});
+
 app.delete("/", async (req, res) => {
   try {
     if (req.query.idUser == "") {
@@ -357,7 +425,8 @@ app.post("/login", async (req, res) => {
            phonenumber: user.phonenumber,
            userprofile: user.userprofile,
            account: user.account,
-           IDcampus: user.IDcampus
+           IDcampus: user.IDcampus,
+           IDrole: user.IDrole
          };
  
          // SIGN TOKEN
@@ -378,7 +447,8 @@ app.post("/login", async (req, res) => {
                phonenumber: user.phonenumber,
                userprofile: user.userprofile,
                account: user.account,
-               IDcampus: user.IDcampus
+               IDcampus: user.IDcampus,
+               IDrole: user.IDrole
              });
            }
          );
